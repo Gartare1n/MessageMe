e94aa58f64283399db4f1afe87b2093a059c40c3
Mono <snayper00001@gmail.com>
2022-05-25 21:10:13 +0300

    Add navbar and favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import '@doabit/semantic-ui-sass';
+import 'jquery'
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
+
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
